e9d30630fc71144336ccc3b0e12021de11ef5d3a
Dariusz Mach <mach.dariusz@gmail.com>
2020-09-13 15:13:03 +0200

    add employee form control

--- web-app/src/app/pages/employees/employee-form/employee-form.component.ts
+++ web-app/src/app/pages/employees/employee-form/employee-form.component.ts
@@ -1,7 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { PageModel } from '../../../shared/model';
+import { PageModel, PAGE_MODE } from '../../../shared/model';
 import { getPageModel } from '../../../shared/utils';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Store } from '@ngxs/store';
+import { EmployeesState } from '../../../shared/state/employees/employees.state';
 
 @Component({
   selector: 'app-employee-form',
@@ -11,13 +14,46 @@ import { getPageModel } from '../../../shared/utils';
 export class EmployeeFormComponent implements OnInit {
 
   pageModel: PageModel;
+  employeeForm: FormGroup;
 
   constructor(
     private _route: ActivatedRoute,
+    private _fb: FormBuilder,
+    private _store: Store,
   ) { }
 
   ngOnInit(): void {
     this.pageModel = getPageModel(this._route);
+    this.initForm();
+  }
+
+  private initForm(): void {
+
+    this.employeeForm = this._fb.group({
+      id: [],
+      firstName: [null, Validators.required],
+      lastName: [null, Validators.required],
+      dateOfBirth: [],
+      phoneNumber: [],
+
+      address: this._fb.group({
+        street: [''],
+        city: [''],
+        state: [''],
+        zip: ['']
+      }),
+    });
+
+    if (this.pageModel.pageMode !== PAGE_MODE.CREATE) {
+      const employee = this._store.selectSnapshot(EmployeesState.getEmployeeById)(this.pageModel.id);
+      console.log('employee: ', employee);
+      this.employeeForm.patchValue(employee);
+    }
+
+    if (this.pageModel.pageMode === PAGE_MODE.DETAILS) {
+      this.employeeForm.disable();
+    }
+
   }
 
 }
--- web-app/src/app/pages/employees/employees.module.ts
+++ web-app/src/app/pages/employees/employees.module.ts
@@ -4,13 +4,15 @@ import { CommonModule } from '@angular/common';
 import { EmployeesRoutingModule } from './employees-routing.module';
 import { EmployeesComponent } from './employees.component';
 import { EmployeeFormComponent } from './employee-form/employee-form.component';
+import { SharedModule } from '../../shared/shared.module';
 
 
 @NgModule({
   declarations: [EmployeesComponent, EmployeeFormComponent],
   imports: [
     CommonModule,
-    EmployeesRoutingModule
+    EmployeesRoutingModule,
+    SharedModule,
   ]
 })
 export class EmployeesModule { }
